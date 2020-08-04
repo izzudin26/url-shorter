3e61382c0ac9f55bef5f65be35e10f86dd3da560
izzudin26 <azidarmor26@gmail.com>
2020-08-05 02:41:00 +0700

    add views

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import { connect, checkConnection } from "./Database/db";
 import bodyParse from "body-parser";
 import controller from "./controller/urlController";
+import path from "path";
 const port = process.env.PORT || 8080;
 
 class App {
@@ -17,15 +18,19 @@ class App {
     checkConnection();
   }
   plugin(): void {
+    this.app.use(express.static("views"));
     this.app.use(bodyParse.json());
   }
   routes(): void {
+    this.app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname + "/views/index.html"));
+    });
     this.app.get("/:url", controller.findUrl);
     this.app.post("/store-url", controller.storeUrl);
   }
 }
 
-let app = new App().app;
-app.listen(port, () => {
+let server = new App().app;
+server.listen(port, () => {
   console.log(`Running on http://localhost:${port}/`);
 });
